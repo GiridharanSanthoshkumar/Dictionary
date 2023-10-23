852c789f567d2cd43712fd41c15593f06061892e
mac <mac@macs-MacBook-Air.local>
2023-10-23 13:26:06 +0530

    everything completed

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/",(req,res)=>{
+    res.render("index.ejs");
+});
+app.get("/content",(req,res)=>
+{
+    res.render("content.ejs");
+
+}); 
+app.post("/api",async(req,res)=>
+{
+    let word=req.body.word;
+    console.log(word);
+    try{
+    const result=await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/"+word);
+    let list=result.data[0].meanings;
+    
+    var sound1;
+    if(result.data[0].phonetics[0])
+      sound1=result.data[0].phonetics[0].audio;
+    else{
+        sound1='';
+    }
+    
+    
+    
+   console.log(sound1);
+    res.render("content.ejs",{meaning:list,wrd:word,sound:sound1});
+    }
+    catch(error)
+    {
+        res.render("content.ejs",{er:error.response})
+    }
+
+});
+
+app.listen(3000,()=>{
+    console.log("server is started at the port 3000!");
+})
